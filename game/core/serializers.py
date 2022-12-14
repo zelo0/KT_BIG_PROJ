a54300de4babffb74457ee80b219d20a06a8d242
Soyuen-Yu <101792456+Soyuen-Yu@users.noreply.github.com>
2022-12-14 15:14:44 +0900

    안티앨리어싱, 상점에 돈 추가

--- game/core/serializers.py
+++ game/core/serializers.py
@@ -4,4 +4,4 @@ from .models import User
 class CurrentUserSerializer(serializers.ModelSerializer):
   class Meta:
     model = User
-    fields = ['username', 'profile_image']
+    fields = ['username', 'profile_image', 'money']
