2c71a909a7d7cdc2170fa0d13d543de8ae4a612e
Soyuen-Yu <101792456+Soyuen-Yu@users.noreply.github.com>
2022-12-15 14:29:15 +0900

    main

--- game/core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index),
-    path('recommend/', views.recommend),
-    path('api/user/', views.CurrentUserAPI.as_view())
-]
