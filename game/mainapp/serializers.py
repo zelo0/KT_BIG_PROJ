5c9264dc2960a8018af9d1b70024eeca0b54642d
minji <3dlalswl3@naver.com>
2022-12-20 22:14:48 +0900

    ItemSerializer 수정

--- game/mainapp/serializers.py
+++ game/mainapp/serializers.py
@@ -1,12 +1,41 @@
 from rest_framework import serializers
-from .models import Shop, Item
+from .models import Shop, Item, ItemImage
 
 class ShopSerializer(serializers.ModelSerializer):
   class Meta:
     model = Shop
     fields = '__all__'
     
+# class ItemSerializer(serializers.ModelSerializer):
+#   class Meta:
+#     model = Item
+#     fields = '__all__'
+
+
+class ItemImageSerializer(serializers.ModelSerializer):
+
+    image = serializers.ImageField(use_url=True)
+
+    class Meta:
+        model = ItemImage
+        fields = ['image']
+
+
 class ItemSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = Item
-    fields = '__all__'
+    images = serializers.SerializerMethodField()
+
+    def get_images(self, obj):
+        image = obj.itemimage_set.all()
+        return ItemImageSerializer(instance=image, many=True).data
+
+    class Meta:
+        model = Item
+        fields = ['id', 'name', 'type', 'images']
+        
+        
+    def create(self, validated_data):
+        instance = Item.objects.create(**validated_data)
+        image_set = self.context['request'].FILES
+        for image_data in image_set.getlist('image'):
+            ItemImage.objects.create(item=instance, image=image_data)
+        return instance
