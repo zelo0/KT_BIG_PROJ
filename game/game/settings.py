94c875777bcc7002cbb4e32e7dd04c0f5875ced1
zelo0 <minorSunbi@gmail.com>
2022-12-22 13:58:20 +0900

    chore: 배포 환경 세팅

--- game/game/settings.py
+++ game/game/settings.py
@@ -123,7 +123,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     # whitenoise
-    # 'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'game.urls'
@@ -165,21 +165,6 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-
-    # mysql
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql', # mysql 엔진 설정
-    #     'NAME': 'kt', # 데이터베이스 이름
-    #     'USER': 'cos', # 데이터베이스 연결시 사용할 유저 이름
-    #     'PASSWORD': 'cos1234', # 유저 패스워드
-    #     'HOST': '127.0.0.1',
-    #     'PORT': '3306',
-    #     'OPTIONS': {
-    #         'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-    #         'charset': 'utf8',
-    #         'use_unicode': True,
-    #     },
-    # }
 }
 
 
@@ -223,13 +208,13 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'config', 'static')
 STATIC_URL = '/static/'
-# for serving static files
+## for serving static files
 ## whitenoise
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 ## 배포 시 제거
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 # Media Files
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
