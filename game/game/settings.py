3707d5745ec2ead055beae9dd84836fadf1b1d18
zelo0 <minorSunbi@gmail.com>
2022-12-26 17:46:22 +0900

    chore: 배포 환경 postgress 연결

--- game/game/settings.py
+++ game/game/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -160,13 +161,13 @@ CHANNEL_LAYERS = {
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    # sqlite
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    ### sqlite
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+    'default': dj_database_url.parse('postgres://runnig_hi_db_user:UtSu9lOKggJVu62U5Aqf2E4e5WIHOYO7@dpg-cefainsgqg4b3hao43lg-a.singapore-postgres.render.com/runnig_hi_db', conn_max_age=600),
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
