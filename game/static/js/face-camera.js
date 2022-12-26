cc5d25f4d38b7b8ea058e99027b40283584dccb8
zelo0 <minorSunbi@gmail.com>
2022-12-26 16:57:38 +0900

    feat: 모션 인식, 게임 타이머

--- game/static/js/face-camera.js
+++ game/static/js/face-camera.js
@@ -15,46 +15,19 @@ function startVideo() {
   );
 }
 
-video.addEventListener("play", () => {
-  const canvas = faceapi.createCanvasFromMedia(video);
-  canvas.id = "match-dimension";
-  document.body.append(canvas);
+function listToMatrix(list, elementsPerSubArray) {
+  var matrix = [],
+    i,
+    k;
 
-  const displaySize = { width: video.width, height: video.height };
-  faceapi.matchDimensions(canvas, displaySize);
-
-  setInterval(async () => {
-    const detections = await faceapi
-      .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
-      .withFaceLandmarks();
-    // .withFaceExpressions(); // 주석
-    const resizedDetections = faceapi.resizeResults(detections, displaySize);
-    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
-    // faceapi.draw.drawDetections(canvas, resizedDetections); // 주석
-    // faceapi.draw.drawFaceLandmarks(canvas, resizedDetections); // 주석
-    // faceapi.draw.drawFaceExpressions(canvas, resizedDetections); // 주석
-
-    // const test3 = resizedDetections[0].landmarks._positions.map(e => e._x);
-    // const test4 = resizedDetections[0].landmarks._positions.map(r => r._y);
-
-    try {
-      const test3 = resizedDetections[0].landmarks._positions; // 예외 발생
-      // console.log(test3, test4)
-      // localStorage.car = JSON.stringify(test3);
-      const jsonData = JSON.stringify(test3);
-      // console.log(jsonData); // 확인해보니 그림은 크게 나오지만 좌표에는 이상 없음
-
-      function saveAsFile(str, filename) {
-        var hiddenElement = document.createElement("a");
-        hiddenElement.href = "data:attachment/text," + encodeURI(str);
-        hiddenElement.target = "_blank";
-        hiddenElement.download = filename;
-        hiddenElement.click();
-      }
-    } catch (e) {
-      const warningText = this.add.text(300, 300, "miss"); // 미완성
+  for (i = 0, k = -1; i < list.length; i++) {
+    if (i % elementsPerSubArray === 0) {
+      k++;
+      matrix[k] = [];
     }
 
-    // saveAsFile(jsonData, "output.json");
-  }, 100); // 현재 0.1초
-});
+    matrix[k].push(list[i]);
+  }
+
+  return matrix;
+}
