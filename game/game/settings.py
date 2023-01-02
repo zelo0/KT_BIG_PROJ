19444c75a2636cc8b6cafe7079293405707f93cf
zelo0 <minorSunbi@gmail.com>
2023-01-02 13:53:25 +0900

    chore: db 변경(배포)

--- game/game/settings.py
+++ game/game/settings.py
@@ -162,11 +162,11 @@ CHANNEL_LAYERS = {
 
 DATABASES = {
     ### sqlite
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-    # 'default': dj_database_url.parse('postgres://runnig_hi_db_user:UtSu9lOKggJVu62U5Aqf2E4e5WIHOYO7@dpg-cefainsgqg4b3hao43lg-a.singapore-postgres.render.com/runnig_hi_db', conn_max_age=600),
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+    'default': dj_database_url.parse('postgres://runnig_hi_db_user:UtSu9lOKggJVu62U5Aqf2E4e5WIHOYO7@dpg-cefainsgqg4b3hao43lg-a.singapore-postgres.render.com/runnig_hi_db', conn_max_age=600),
 }
 
 # Password validation
